c6d2eb4a4cf65055fff74ae98638abbedde6a261
s18844 <s18844@LAB220-05>
2020-03-05 11:11:57 +0100

    Regex Web

--- Cw1/Cw1/Program.cs
+++ Cw1/Cw1/Program.cs
@@ -1,11 +1,29 @@
 ﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace Cw1
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
+            var client = new HttpClient();
+            var result = await client.GetAsync("https://www.pja.edu.pl");
+            //ThreadPool() task - kiedys tam bedzie wynik - await mowi ze poczekaj az tam sie znajdzie cos - do main dodajemy async Task - wymagane do await
+            if(result.IsSuccessStatusCode) //2xx
+            {
+                string html = await result.Content.ReadAsStringAsync(); //Bo result moze byc duzy i serwer bedzie dzielil i wysylal pakietami
+                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z.]+");
+                var matches = regex.Matches(html);
+                foreach(var m in matches)
+                {
+                    Console.WriteLine(m);
+                }
+            }
+
+
             Console.WriteLine("Hello World!");
         }
     }
